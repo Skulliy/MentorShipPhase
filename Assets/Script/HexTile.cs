1167c7727a0f2886f4c52639e3cb5a8d34183455
whitewolf18 <169000698+whitewolf18@users.noreply.github.com>
2025-11-18 20:42:47 +0200

    my first upload

---
+++ Assets/Script/HexTile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile : MonoBehaviour
+{
+    public bool playerOnTile = false;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerOnTile = true;
+
+            if (rend != null)
+                rend.material.color = Color.green;
+
+            Debug.Log(gameObject.name + " → PLAYER ON TILE");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerOnTile = false;
+
+            
+            if (rend != null)
+                rend.material.color = Color.white;
+
+            Debug.Log(gameObject.name + " → PLAYER LEFT TILE");
+        }
+    }
+}
